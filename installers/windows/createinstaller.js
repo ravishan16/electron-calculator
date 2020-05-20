54edfeaebfc07ae1f3ab5c5d9797a0d5fa1a8bd2
Ravishankar Sivasubramaniam <ravishan16@users.noreply.github.com>
2020-05-20 18:53:51 -0400

    Update createinstaller.js

--- installers/windows/createinstaller.js
+++ installers/windows/createinstaller.js
@@ -2,14 +2,16 @@ var electronInstaller = require('electron-winstaller');
 // In this case, we can use relative paths
 var settings = {
 // Specify the folder where the built app is located
-appDirectory: '/Users/ravis/projects/git/electron-calculator/release-builds/accrual-calculator-win32-x64',
+appDirectory: "./release-builds/accrual-calculator-win32-x64",
 // Specify the existing folder where
-outputDirectory: '/Users/ravis/projects/git/electron-calculator/windows-installer',
+outputDirectory: "./installers",
 // The name of the Author of the app (the name of your company)
 authors: 'Ravi',
 // The name of the executable of your built
-exe: '/Users/ravis/projects/git/electron-calculator/release-builds/accrual-calculator-win32-x64/accrual-calculator.exe',
-description: 'Accrual Calculator'
+exe: "./accrual-calculator.exe",
+description: 'Accrual Calculator',
+noMsi: true,
+setupExe: 'AccrualCalculator.exe'
 };
 resultPromise = electronInstaller.createWindowsInstaller(settings);
 resultPromise.then(() => {
